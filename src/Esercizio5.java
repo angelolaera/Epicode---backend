c9d6793b3b55dcf8aa9c647105f6ec67af82483f
angelolaera <angelo.laera.1995@gmail.com>
2024-07-22 17:54:47 +0200

    Update Esercizio5.java

--- src/Esercizio5.java
+++ src/Esercizio5.java
@@ -20,6 +20,15 @@ public class Esercizio5 {
         }
 
 
+        System.out.println("Esercizio perimetroTriangolo");
+        System.out.println("Inserisci primo valore:");
+        double val1 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Inserisci secondo valore:");
+        double val2 = Double.parseDouble(scanner.nextLine());
+        System.out.println("Inserisci terzo valore:");
+        double val3 = Double.parseDouble(scanner.nextLine());
+        System.out.println("L'area del nostro triangoolo è:" + areaTriangolo(val1,val2,val3));
+
     }
 
     public static double perimetroRettangolo(double lato1, double lato2){
@@ -30,7 +39,7 @@ public class Esercizio5 {
         return num1 % 2;
     }
 
-    public static double perimetroTriangolo(double l1, double l2, double l3) {
+    public static double areaTriangolo(double l1, double l2, double l3) {
         double p = (l1 + l2 + l3) / 2;
         return Math.sqrt(p * (p - l1) * (p - l2) * (p - l3));
     }
